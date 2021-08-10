8d462b245d3f36a5ca32c32eb9f4f8d69eead6d7
iGomezP <i.gomezp@outlook.com>
2021-08-10 16:41:39 -0500

    Attend new errors

--- Challenge1/react_solution/src/components/Footer.jsx
+++ Challenge1/react_solution/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer role="contentinfo" className="attribution">
+    <footer className="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>.
       Coded by <a href="https://github.com/iGomezP" target="_blank" rel='noreferrer'>Ignacio Gomez Puga</a>.
     </footer>
